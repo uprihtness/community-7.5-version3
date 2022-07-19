36e24f76900d59106f79a0e04656ed0a2bd28c8f
uprightness <uprightness@163.com>
2022-07-19 21:36:26 +0800

    第3.6小节发布帖子

---
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -0,0 +1,51 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping("/discuss")
+public class DiscussPostController {
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    // 页面访问路径
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    /**
+     * 页面传入title(标题)和content(内容)两种数据
+     */
+    public String addDiscussPost(String title, String content) {
+        // 先判断用户是否登录了
+        User user = hostHolder.getUser();
+        if (user == null) {
+            return CommunityUtil.getJSONString(403, "你还没有登录哦!");
+        }
+
+        // 用户登录了，继续往下处理
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle(title);
+        post.setContent(content);
+        post.setCreateTime(new Date());
+        discussPostService.addDiscussPost(post);
+
+        // 报错的情况,将来统一处理.
+        return CommunityUtil.getJSONString(0, "发布成功!");
+    }
+
+}
