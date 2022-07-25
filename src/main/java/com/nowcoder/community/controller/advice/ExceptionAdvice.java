c8e95921888d0d143f0eb845a639312d73bc0b63
uprightness <uprightness@163.com>
2022-07-25 11:45:02 +0800

    第3.33小节统一记录日志

---
+++ src/main/java/com/nowcoder/community/controller/advice/ExceptionAdvice.java
@@ -0,0 +1,39 @@
+package com.nowcoder.community.controller.advice;
+
+import com.nowcoder.community.util.CommunityUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@ControllerAdvice(annotations = Controller.class)
+public class ExceptionAdvice {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionAdvice.class);
+
+    @ExceptionHandler({Exception.class})
+    public void handleException(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        logger.error("服务器发生异常: " + e.getMessage());
+        for (StackTraceElement element : e.getStackTrace()) {
+            logger.error(element.toString());
+        }
+
+        // 用来判断请求方式
+        String xRequestedWith = request.getHeader("x-requested-with");
+        // 返回的是XML，因此是异步请求。（异步请求返回：XML、json; 同步请求返回HTML)
+        if ("XMLHttpRequest".equals(xRequestedWith)) {
+            response.setContentType("application/plain;charset=utf-8");
+            PrintWriter writer = response.getWriter();
+            writer.write(CommunityUtil.getJSONString(1, "服务器异常!"));
+        } else {
+            response.sendRedirect(request.getContextPath() + "/error");
+        }
+    }
+
+}
